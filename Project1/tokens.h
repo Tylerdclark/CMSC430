db62f1a0a232efc01089dfede9b2df67f7600064
Tyler Clark <tylerc.thedeveloper@gmail.com>
2021-10-24 12:51:47 -0400

    initial code

---
+++ Project1/tokens.h
@@ -0,0 +1,7 @@
+// Compiler Theory and Design
+// Dr. Duane J. Jarc
+
+// This file contains the enumerated type definition for tokens
+
+enum Tokens {RELOP = 256, ADDOP, MULOP, ANDOP, BEGIN_, BOOLEAN, END, ENDREDUCE,
+	FUNCTION, INTEGER, IS, REDUCE, RETURNS, IDENTIFIER, INT_LITERAL};
